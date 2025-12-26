6e067191f389563f7a670dc2cb5577495ce562de
nicius2 <marco.marks06@gmail.com>
2025-12-26 13:41:07 -0300

    fix(firebase): Carrega credenciais a partir de variável de ambiente

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,17 +1,18 @@
 import admin from 'firebase-admin';
-import type { ServiceAccount } from 'firebase-admin';
 
-// Verifica a variável
+import 'dotenv/config';
+
 if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
-  throw new Error('A variável FIREBASE_SERVICE_ACCOUNT não está definida.');
+  throw new Error(
+    'A variável de ambiente FIREBASE_SERVICE_ACCOUNT não está definida.',
+  );
 }
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-// ✅ ADICIONE ISSO: Verifica se já não existe uma instância rodando
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
