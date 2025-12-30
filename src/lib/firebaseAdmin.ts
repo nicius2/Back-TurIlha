959fb9854b07f32f571dcc68379a8a5b2606a9f8
nicius2 <marco.marks06@gmail.com>
2025-12-30 15:11:45 -0300

    fix: env firebase

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,9 +1,9 @@
 import admin from 'firebase-admin';
 import type { ServiceAccount } from 'firebase-admin';
-import serviceAccount from "../../turilha-firebase-adminsdk.json"; // Importa o JSON como um objeto
+import "dotenv/config";
 
 // Converte o JSON importado para o tipo ServiceAccount
-const firebaseConfig = serviceAccount as ServiceAccount;
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG as string) as ServiceAccount;
 
 // ✅ Verifica se já não existe uma instância rodando
 if (!admin.apps.length) {
